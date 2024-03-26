90ca12d47f7ba004faccca444cdffacf82782d24
Harsh Dabas <iamharshdabas@gmail.com>
2024-03-26 18:44:08 +0530

    chore: update map default zoom

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -46,7 +46,7 @@ const LeafletMap = () => {
 
   return (
     <div className="fixed left-0 top-0 h-screen w-screen">
-      <MapContainer className="h-full" center={mapPosition} zoom={3}>
+      <MapContainer className="h-full" center={mapPosition} zoom={5}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
